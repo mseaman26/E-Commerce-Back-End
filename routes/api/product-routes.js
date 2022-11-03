9aa4c0fea9662f7f9d1ce7670d0240ed6bff0131
michael seaman <mseaman26@gmail.com>
2022-11-03 16:50:01 -0700

    did first product route.  working on second

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,18 +1,41 @@
 const router = require('express').Router();
+// const { json } = require('sequelize/types');
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try{
+    const allProducts = await Product.findAll({
+      include: [{model: Category},{model: Tag}],
+
+    })
+    if(!allProducts){
+      res.status(400).json({message: "no products found!"})
+      return
+    }
+    res.status(200).json(allProducts)
+  }catch (err){
+    res.status(500).json(err)
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try{
+    const requestedProduct = await Product.findByPk(req.params.id, {
+      include: [{model: Category}, {model: Tag}],
+    })
+    if(!requestedProduct){
+      res.status(400).json({message: "product"})
+    }
+  }
+  
 });
 
 // create new product
