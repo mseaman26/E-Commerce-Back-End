02f626c77941439348905735eb91933c185fb5e0
michael seaman <mseaman26@gmail.com>
2022-11-02 22:26:11 -0700

    wrote most of one route, added gitignore

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,11 +1,25 @@
 const router = require('express').Router();
+const { where } = require('sequelize/types');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try{
+    const categories = await Category.findAll({
+      
+    })
+    if(!categories){
+      res.status(404).json({ message: "no categories found" })
+      return
+    }
+    res.status(200).json(categories)
+  }
+  catch (err){
+    res.status(500).json(err)
+  }
 });
 
 router.get('/:id', (req, res) => {
