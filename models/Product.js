121f464cbac2e56c9173e87a5e916807469a8910
michael seaman <mseaman26@gmail.com>
2022-11-03 19:29:26 -0700

    create new tag route complete

--- models/Product.js
+++ models/Product.js
@@ -23,13 +23,19 @@ Product.init(
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
-      isDecimal: true
+      validate: {
+        isDecimal: true
+      }
+      
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
-      isNumeric: true
+      validate: {
+        isNumeric: true
+      }
+      
 
     },
     category_id:{
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -40,8 +40,17 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create({
+      tag_name: req.body.tag_name
+    })
+    res.status(200).json({message: "your tag has been created!"})
+  }
+  catch (err){
+    res.status(500).json(err)
+  }
 });
 
 router.put('/:id', (req, res) => {
