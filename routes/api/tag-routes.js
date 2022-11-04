ad2c18767f881a7bc0cf95367f1146e9fb0fdb05
michael seaman <mseaman26@gmail.com>
2022-11-03 19:35:02 -0700

    update tag route complete

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -49,12 +49,26 @@ router.post('/', async (req, res) => {
     res.status(200).json({message: "your tag has been created!"})
   }
   catch (err){
-    res.status(500).json(err)
+    res.status(400).json(err)
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updatedTag = Tag.update({
+      tag_name: req.body.tag_name
+    },
+    {
+      where:{
+        id: req.params.id
+      }
+    })
+    res.status(200).json({message: "tag has been updated!"})
+  }
+  catch (err){
+    res.status(400).json(err)
+  }
 });
 
 router.delete('/:id', (req, res) => {
