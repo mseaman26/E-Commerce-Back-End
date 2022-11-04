2bd6456d5ba161be05d96366e33dc670774fcfc6
michael seaman <mseaman26@gmail.com>
2022-11-03 17:27:55 -0700

    delete product by id completed

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -118,8 +118,18 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try{
+    const deletedProduct = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.status(200).json({message: "product deleted!"})
+  }catch(err){
+    res.status(400).json(err)
+  }
 });
 
 module.exports = router;
